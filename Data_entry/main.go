a0187f882af02a5ce0cce21b7cd0c3067c05b516
Muhammad Rezaul Karim <77927449+mtmart@users.noreply.github.com>
2021-03-24 16:50:35 +0600

    new data entry page by golang

---
+++ Data_entry/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", home)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8888", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+
+}
