a59b88a244d7a975b31b0f37c752ed13d37a4e54
Muhammad Rezaul Karim <77927449+irezaul@users.noreply.github.com>
2021-08-17 22:09:23 +0600

    fmt.Sprintf() Function

---
+++ fmt.Sprintf() Function/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	//var name string
+	name, Academy := "Rezaul Karim", "DevOps"
+
+	show := fmt.Sprintf("%s is a student of %s Master-Academy. \n", name, Academy)
+
+	io.WriteString(os.Stdout, show)
+}
