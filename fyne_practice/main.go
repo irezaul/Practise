1e3c3696a78686a19fb1159ef1253344d3c33440
Muhammad Rezaul Karim <77927449+irezaul@users.noreply.github.com>
2022-01-13 01:03:23 +0600

    fyne practise

---
+++ fyne_practice/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+
+	//New App
+	myapp := app.New()
+
+	// windows with title
+	window := myapp.NewWindow("miniERP V0.0.1")
+
+	// Windows resize of app
+	window.Resize(fyne.NewSize(600, 400))
+
+	// label call
+	label1 := widget.NewLabel("Welcome to new miniERP")
+
+	//set contain
+	window.SetContent(label1)
+	// running and show
+	window.ShowAndRun()
+}
